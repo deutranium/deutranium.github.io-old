6c1e0fe88e16b8522879065a32ef823dbe2446a1
deutranium <29124655+deutranium@users.noreply.github.com>
2020-11-10 23:57:57 +0530

    Added descriptions

--- js/theme.js
+++ js/theme.js
@@ -418,21 +418,35 @@
 
 })(jQuery)
 
+let project_descriptions = {
+    "discord": `Developed a discord bot to manage transactions by 20 organizers for 71 participants for a college event <br><br>The bot was created using Discord.py and connected to sqlite for data storage, hosted and used in the event server with options to view the scoreboard, change scores, view history etc.`,
+    "dbms": ` Collaborated on a command line applications to be used to perform CRUD operations on a database of a publication company <br><br>The CLI had functionalities like creating new articles and issues, change their details like author, editor, retrieve results after applying filters, analyze the change in subscribers, check sponsor and contributer details etc.`,
+    "ubuntu": `Created a desktop app for WhatsApp to be used in linux distros using the chromium command line application and ubuntu .desktop configurations`,
+    "wa": ` Building a flask app as a way to summarize WhatsApp chats in a visual way <br><br>The application uses pandas to retrieve dataframes from the WhatsApp chats, regex to clean data and the wordcloud library for visualisation`,
+    "heibi": `Making a cross platform flutter app linked with Firebase for data storage and easy access, as a part of a team of 7 <br><br>The app aims to make day to day functions for college student easier with features like managing laundry, getting food from food courts, using amenities like TT, badminton courts etc.`,
+    "joystick": `Created a river crossing game as a part of Intro to Software Systems course in Python, and using the PyGame library <br><br>This is a two-player game, with increasing difficulty as the game progresses. The aim is to dodge the moving and fixed obstacles and reach the other end.`,
+    "japan": `Created a website initially aimed to help in my journey of learning the 46 alphabets of the hiragana script of the Japanese language<br><br>Displays random hiragana alphabets with blanks to enter the pronounciation, giving options to hear it and display the score in the end`,
+    "todo": `Designed a ToDo List App in Java connected to a MySQL database for data storage<br><br>The features include creation of new tasks, modification of the old taks, along with an option to delete them`,
+    "filter": `Designed a cutoff filter to help choosing colleges during JoSAA counselling 2019 based on different filters like opening ranks, closing ranks, categories etc.`
+}
 
-function henlo(txt){
+
+function henlo(txt, img){
     Swal.fire({
             html: txt,
             showCloseButton: true,
-            showConfirmButton: false
+            showConfirmButton: false,
+            focusConfirm: false,
+            imageUrl: img
         }
     )
 }
 
 function generateHtml(title, tech, desc, img, gh = "#", lnk = "#"){
-    final = ""
-    gh_shiz = ""
-    lnk_shiz = ""
-    social_shiz = ""
+    let final = ""
+    let gh_shiz = ""
+    let lnk_shiz = ""
+    let social_shiz = ""
 
     if(gh != "#"){
         gh_shiz = `<a href="` + gh + `"><i class="fa fa-github fa-2x"></i></a>`
@@ -451,14 +465,17 @@ function generateHtml(title, tech, desc, img, gh = "#", lnk = "#"){
     final = `
             <div class="blog_right_sidebar">
                 <aside class="single_sidebar_widget author_widget">
-                    <img class="author_img rounded-circle popup-img" src="img/gallery/` + img + `.png" alt="">
+                    <!--<img class="author_img rounded-circle popup-img" src="img/gallery/` + img + `.png" alt="">-->
                     <h4>` + title + `</h4>
                     <p>` + tech + `</p>
                     ` + social_shiz + `
-                    <p>` + desc + `</p>
+                    <p>` + project_descriptions[img] + `</p>
                     <div class="br"></div>
                 </aside>
             </div>`
 
-    henlo(final)
+    img = "img/gallery/" + img + ".png"
+
+
+    henlo(final, img)
 }
